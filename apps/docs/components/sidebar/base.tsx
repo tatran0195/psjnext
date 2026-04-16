c9e0a2ae115070b2de84ee69380515575437a682
akaw <tvta0195@gmail.com>
2026-04-17 00:53:37 +0700

    fix: resolved module not found

--- apps/docs/components/sidebar/base.tsx
+++ apps/docs/components/sidebar/base.tsx
@@ -14,7 +14,7 @@ import {
 } from 'react';
 import Link, { type LinkProps } from 'fumadocs-core/link';
 import { useOnChange } from 'fumadocs-core/utils/use-on-change';
-import { cn } from '../../lib/cn';
+import { cn } from '@/lib/cn';
 import {
     Collapsible,
     CollapsibleContent,
